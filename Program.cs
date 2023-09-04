b876d2b7cdcf4a0e6f54f76236abe5bb48933f63
sananeo0 <sananeo@code.edu.az>
2023-09-04 17:33:44 +0400

    end

--- Program.cs
+++ Program.cs
@@ -344,7 +344,22 @@ namespace LinqTask
             }
             */
 
+            string[] cities =
+            {
+                "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
+            };
+            Console.Write("\nLINQ : Display the list according to the length then by name in ascending order : ");
+            Console.Write("\n--------------------------------------------------------------------------------\n");
+            Console.Write("\nThe cities are : 'ROME','LONDON','NAIROBI','CALIFORNIA','ZURICH','NEW DELHI','AMSTERDAM','ABU DHABI','PARIS' \n");
+            Console.Write("\nHere is the arranged list :\n");
+            IEnumerable<string> cityOrder = cities
+           .OrderBy(str => str.Length)
+           .ThenBy(str => str);
 
+            foreach (string city in cityOrder)
+            {
+                Console.WriteLine(city);
+            }
 
         }
 
